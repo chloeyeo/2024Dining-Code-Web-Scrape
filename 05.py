c3b19743617fe3fecc95ed83960a8078ce127a11
Chloe Yeo <yeochloe01@gmail.com>
2024-05-23 18:08:16 +0900

    close_ad and location btns not found until timeout

--- 05.py
+++ 05.py
@@ -4,6 +4,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 options = Options()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
@@ -26,6 +29,19 @@ item_info = driver.find_element(By.XPATH, '//*[@id="root"]/div/main/div[2]/div[3
 item_info.click()
 
 data = []
+
+wait = WebDriverWait(driver, 10)
+
+
+try: 
+    close_ad = wait.until(EC.visibility_of_element_located((By.ID, "dismiss-button")))
+    close_ad.click()
+
+    location = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "locat")))
+    print("location:", location)
+
+except TimeoutException:
+    print("close_ad and location buttons not found within the timeout.")
 
 # df = pd.DataFrame(data, columns=[])
 
