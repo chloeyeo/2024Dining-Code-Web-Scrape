14090ee56dbf036d4e290337a385cb85803a960e
Chloe Yeo <yeochloe01@gmail.com>
2024-05-27 12:40:20 +0900

    enabled ad blocking, however there is a problem of keep on opening the tabs of the same restaurant

--- 06.py
+++ 06.py
@@ -9,14 +9,12 @@ import random
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import TimeoutException
 
 options = Options()
-# options.add_argument('--headless')
+options.add_extension("./extensions/ad_block.crx")
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 options.add_experimental_option("detach", True)
 options.add_argument("--start-maximized")
-options.add_argument("--incognito")
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
@@ -24,6 +22,20 @@ driver = webdriver.Chrome(service=service, options=options)
 data = []
 
 driver.get("https://www.diningcode.com/")
+
+rand = random.uniform(1, 5)
+
+time.sleep(rand)  # wait for 5 seconds
+driver.implicitly_wait(rand)  # wait until the web page shows up
+main = driver.window_handles
+# print("main:", main) # opened tabs
+for i in main:
+    if i != main[0]:
+        driver.switch_to.window(i)
+        driver.close()
+time.sleep(rand)
+driver.implicitly_wait(rand)
+driver.switch_to.window(driver.window_handles[0])
 
 # Wait until the element is clickable
 wait = WebDriverWait(driver, 10)
@@ -68,69 +80,45 @@ for i in range(20):
         Keys.END
     )  # 모든 데이타가 다 불러져 온 상태에서 크롤링을 하려고 끝까지 페이지를 내린거다.
 
-rand = random.uniform(1, 5)
-time.sleep(rand)  # wait for 5 seconds
-driver.implicitly_wait(rand)  # wait until the web page shows up
+
+time.sleep(rand)
+driver.implicitly_wait(rand)
 
 # 서울 전체지역 안에 있는 음식점들
-# wait2 = WebDriverWait(driver, 20)
 restaurants = wait.until(
     EC.presence_of_all_elements_located((By.CLASS_NAME, "Slide__Card__Item"))
 )
-# restaurants is a single web element
-print("Type of restaurants:", type(restaurants))
-# this differs each time I run
+
+# print("Type of restaurants:", type(restaurants))
 print("Number of restaurants found:", len(restaurants))
+print("restaurants:", restaurants)
 
 for restaurant in restaurants:
-    try:
-        close_ad_btn = wait.until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "#aswift_1 #dismiss-button")
-            )
-        )
-        close_ad_btn.click()
-    except TimeoutException:
-        print(
-            "Dismiss button is not clickable or visible. Proceeding without clicking. - 1"
-        )
+    time.sleep(rand)
+    driver.implicitly_wait(rand)
+
     class_names = restaurant.get_attribute("class").split()
-    # .sc-ppyJt.eckjkM.Slide__Card__Item a
     css_selector = "." + ".".join(class_names) + " a"
     restaurantATag = wait.until(
         EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
     )
 
-    try:
-        close_ad_btn = wait.until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "#aswift_1 #dismiss-button")
-            )
-        )
-        close_ad_btn.click()
-    except TimeoutException:
-        print(
-            "Dismiss button is not clickable or visible. Proceeding without clicking. - 2"
-        )
+    time.sleep(rand)
+    driver.implicitly_wait(rand)
+
     # Scroll to the element to ensure it's in view
     driver.execute_script("arguments[0].scrollIntoView();", restaurantATag)
+
+    time.sleep(rand)
+    driver.implicitly_wait(rand)
 
     restaurantATag.click()
 
     # Switch to the newly opened tab
     driver.switch_to.window(driver.window_handles[-1])
 
-    try:
-        close_ad_btn = wait.until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "#aswift_1 #dismiss-button")
-            )
-        )
-        close_ad_btn.click()
-    except TimeoutException:
-        print(
-            "Dismiss button is not clickable or visible. Proceeding without clicking. - 3"
-        )
+    time.sleep(rand)
+    driver.implicitly_wait(rand)
 
     name = wait.until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "#div_profile .tit"))
@@ -150,18 +138,15 @@ for restaurant in restaurants:
 
     # Switch to the first tab
     driver.switch_to.window(driver.window_handles[0])
-    try:
-        close_ad_btn = wait.until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "#aswift_1 #dismiss-button")
-            )
-        )
-        close_ad_btn.click()
-    except TimeoutException:
-        print(
-            "Dismiss button is not clickable or visible. Proceeding without clicking. - 4"
-        )
+
+    time.sleep(rand)
+    driver.implicitly_wait(rand)
+
+main = driver.window_handles
+for i in main:
+    driver.switch_to.window(i)
+    driver.close()
 
 print("data:", data)
-# df = pd.DataFrame(data, columns=[metropolitan, city, district, detailedAddress])
-# df.to_csv("diningcode.csv", index=False)
+df = pd.DataFrame(data, columns=[metropolitan, city, district, detailedAddress])
+df.to_csv("diningcode.csv", index=False)
